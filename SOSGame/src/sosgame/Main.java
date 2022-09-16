cb5f7ca66a776784a15280111bb8eb7e5892770e
unknown <jafleeger@gmail.com>
2022-09-16 18:57:02 -0500

    Basic elements of Game GUI created.

---
+++ SOSGame/src/sosgame/Main.java
@@ -0,0 +1,23 @@
+package sosgame;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sosgame.fxml"));
+        primaryStage.setTitle("SOS Game");
+        primaryStage.setScene(new Scene(root, 400, 300));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
